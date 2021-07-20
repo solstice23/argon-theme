8123b62089820fc122b9fd36be6ee3be2125f35b
Styunlen <30810222+Styunlen@users.noreply.github.com>
2021-07-20 22:33:50 +0800

    添加了对多页文章的支持
    添加了对多页文章的支持

--- functions.php
+++ functions.php
@@ -1496,6 +1496,64 @@ function the_content_filter($content){
 	return $content;
 }
 add_filter('the_content' , 'the_content_filter',20);
+//文末文章分页显示
+function content_pagesNav($content)
+{
+  /* 使用到的函数
+    _wp_link_page($page),返回文章第$page页的html超链接，类似<a href="...">,但是没有尾标签，所以要手动加上</a>
+	还有用到全局作用域中的一些变量
+  */
+  global $page, $numpages, $multipage, $more;
+  if($multipage && ( is_single() || is_page() ) )//只有在多页的文章或页面中才会显示导航栏
+  {
+    $content .= '<ul class="pagination">';
+    /* 上一页标签 */
+
+    //<li class="page-item disabled">
+    $prev = $page - 1;
+    if($prev > 0)
+    {
+        $link = '<li class="page-item">'. _wp_link_page( $prev ) . '上一页</a>';
+    }
+    else
+    {
+      $link = '<li class="page-item disabled"><a href="#" tabindex="-1">上一页</a>';
+    }
+    $content .= $link . '</li>';
+    
+    /* 文章页码标签 */
+    
+    for ($i = 1; $i <= $numpages; $i++) {
+      	$link = $i ;
+      	if ($i != $page || !$more && 1 == $page) {
+		  	//通过正则表达式为其添加class来匹配主题的样式
+			$pattern_temp = '/class="post-page-numbers"/i';
+			$replacement_temp = 'class="post-page-numbers page-link"';
+			$pregRet = preg_replace($pattern_temp, $replacement_temp, _wp_link_page($i));
+        	$link = '<li class="page-item">' . $pregRet . $link .'</a></li>';
+      	} elseif ($i === $page) {
+        	$link = '<li class="page-item active"><span class="page-link" href="#">' . $link. '</span></li>';     
+      	}
+    	$content .= $link;
+    }
+  
+    /* 下一页标签 */
+
+    $next = $page + 1;
+    if($next <= $numpages)
+    {
+      $link = '<li class="page-item">'. _wp_link_page( $next ) . '下一页</a>';
+    }
+    else
+    {
+      $link = '<li class="page-item disabled"><a href="#" tabindex="-1">下一页</a>';
+    }
+    $content .= $link . '</li>';
+    $content .= '</ul>';
+  }
+  return $content;
+}
+add_filter('the_content', 'content_pagesNav');
 //使用 CDN 加速 gravatar
 function gravatar_cdn($url){
 	$cdn = get_option('argon_gravatar_cdn', 'gravatar.loli.net/avatar/');
