11134ee78925747119cfd9de870adae727aeee6e
solstice23 <solstice233@gmail.com>
2021-07-22 14:15:53 +0800

    update translation

--- functions.php
+++ functions.php
@@ -4356,13 +4356,15 @@ function argon_update_themeoptions(){
 argon_update_themeoptions();
 
 /*主题菜单*/
-register_nav_menus( array(
-	'toolbar_menu' => __('顶部导航', 'argon'),
-	'leftbar_menu' => __('左侧栏菜单', 'argon'),
-	'leftbar_author_links' => __('左侧栏作者个人链接', 'argon'),
-	'leftbar_friend_links' => __('左侧栏友情链接', 'argon')
-));
-
+add_action('init', 'init_nav_menus');
+function init_nav_menus(){
+	register_nav_menus( array(
+		'toolbar_menu' => __('顶部导航', 'argon'),
+		'leftbar_menu' => __('左侧栏菜单', 'argon'),
+		'leftbar_author_links' => __('左侧栏作者个人链接', 'argon'),
+		'leftbar_friend_links' => __('左侧栏友情链接', 'argon')
+	));
+}
 
 //隐藏 admin 管理条
 //show_admin_bar(false);
