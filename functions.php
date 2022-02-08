9173b0e32fcbd3a9187c40204c6b1494b85d93ed
solstice23 <solstice233@gmail.com>
2022-02-09 00:31:33 +0800

    Fix #397

--- functions.php
+++ functions.php
@@ -2996,7 +2996,7 @@ function argon_tinymce_add_plugin($plugins){
 //主题选项页面
 function themeoptions_admin_menu(){
 	/*后台管理面板侧栏添加选项*/
-	add_menu_page(__("Argon 主题设置", 'argon'), __("Argon 主题选项", 'argon'), 'edit_themes', basename(__FILE__), 'themeoptions_page');
+	add_menu_page(__("Argon 主题设置", 'argon'), __("Argon 主题选项", 'argon'), 'edit_theme_options', basename(__FILE__), 'themeoptions_page');
 }
 function themeoptions_page(){
 	/*具体选项*/
