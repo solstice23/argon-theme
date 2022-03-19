435cf0ed68864b39778ffbad79a131b9ec8840df
solstice23 <solstice233@gmail.com>
2022-03-19 20:48:14 +0800

    Minify CSS

---
+++ assets/postcss.config.js
@@ -0,0 +1,23 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+
+module.exports = {
+	plugins: [
+		purgecss({
+			content: [
+				'../*.php',
+				'../*/*.php',
+				'./src/*.js',
+				'./src/*/*.js',
+				'./src/*/*/*.js',
+				'./src/*/*/*/*.js',
+				'./src/*.css',
+				'./src/js/*.css',
+				'./src/js/*/*.css',
+				'./src/js/*/*/*.css',
+				'./node_modules/nouislider/dist/*.css',
+				'./node_modules/bootstrap/js/src/*.js',
+			],
+		})
+	]
+}
