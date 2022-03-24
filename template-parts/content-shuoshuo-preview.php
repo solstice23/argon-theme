97014e89e5631ba9ad8995446d5198e6b0b849e7
solstice23 <solstice233@gmail.com>
2022-03-24 20:18:53 +0800

    Update shuoshuo preview style

--- template-parts/content-shuoshuo-preview.php
+++ template-parts/content-shuoshuo-preview.php
@@ -9,13 +9,18 @@
 	</article>
 	<div class="shuoshuo-preview-meta">
 		<span>
-			<i class="fa fa-calendar" aria-hidden="true"></i> 
-			<span class="shuoshuo-date-month"><?php echo get_the_time('n')?></span> <?php _e('月', 'argon');?> 
-			<span class="shuoshuo-date-date"><?php echo get_the_time('d')?></span> <?php _e('日', 'argon');?> , 
-			<span class="shuoshuo-date-year"><?php echo get_the_time('Y')?></span>
+			
+			<div class="post-meta-detail">
+				<i class="fa fa-calendar-o" aria-hidden="true"></i> 
+				<span class="shuoshuo-date-month"><?php echo get_the_time('n')?></span> <?php _e('月', 'argon');?> 
+				<span class="shuoshuo-date-date"><?php echo get_the_time('d')?></span> <?php _e('日', 'argon');?> , 
+				<span class="shuoshuo-date-year"><?php echo get_the_time('Y')?></span>
+			</div>
 			<div class="post-meta-devide">|</div>
-			<i class="fa fa-clock fa-clock-o" aria-hidden="true"></i> 
-			<span class="shuoshuo-date-time"><?php echo get_the_time('G:i')?></span>
+			<div class="post-meta-detail">
+				<i class="fa fa-clock fa-clock-o" aria-hidden="true"></i> 
+				<span class="shuoshuo-date-time"><?php echo get_the_time('G:i')?></span>
+			</div>
 		</span>
 		<?php if ( is_sticky() ) : ?>
 			<div class="post-meta-devide">|</div>
