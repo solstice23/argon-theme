5e616c2851098c00cb0a3971935ad290ee50326c
solstice23 <solstice233@gmail.com>
2022-04-30 00:37:03 +0800

    Fix fullscreen banner doesn't work

--- assets/src/js/banner-cover.js
+++ assets/src/js/banner-cover.js
@@ -1,49 +1,54 @@
 import { gotoHash } from './utils/go-to-hash';
 var $ = window.$;
-if ($("html").hasClass("banner-as-cover")){
-	function classInit(){
-		if ($("#main").hasClass("article-list-home")){
-			if (!$("html").hasClass("is-home")){
-				$("html").addClass("is-home");
-				$("html").trigger("resize");
-			}
-		}else{
-			if ($("html").hasClass("is-home")){
-				$("html").removeClass("is-home");
-				$("html").trigger("resize");
-			}
+const classInit = () => {
+	if ($("#main").hasClass("article-list-home")){
+		if (!$("html").hasClass("is-home")){
+			$("html").addClass("is-home");
+			$("html").trigger("resize");
+		}
+	}else{
+		if ($("html").hasClass("is-home")){
+			$("html").removeClass("is-home");
+			$("html").trigger("resize");
 		}
 	}
-	classInit();
-	new MutationObserver(function(mutations, observer){
-		classInit();
-	}).observe(document.querySelector("#primary"), {
-		'childList': true
-	});
-	$(".cover-scroll-down").on("click" , function(){
-		gotoHash("#content", 600, 'easeOutCirc');
-		$("#content").focus();
-	});
-	$fabs = $("#float_action_buttons");
-	$coverScrollDownBtn = $(".cover-scroll-down");
-	function changeWidgetsDisplayStatus(){
-		let scrollTop = $(window).scrollTop();
-		if (scrollTop >= window.outerHeight * 0.2){
-			$fabs.removeClass("hidden");
-		}else{
-			$fabs.addClass("hidden");
-		}
-		if (scrollTop >= window.outerHeight * 0.6){
-			$coverScrollDownBtn.addClass("hidden");
-		}else{
-			$coverScrollDownBtn.removeClass("hidden");
-		}
-	}
-	changeWidgetsDisplayStatus();
-	$(window).scroll(function(){
-		changeWidgetsDisplayStatus();
-	});
-	$(window).resize(function(){
-		changeWidgetsDisplayStatus();
-	});
 }
+const changeWidgetsDisplayStatus = () => {
+	let scrollTop = $(window).scrollTop();
+	if (scrollTop >= window.outerHeight * 0.2){
+		$("#float_action_buttons").removeClass("hidden");
+	}else{
+		$("#float_action_buttons").addClass("hidden");
+	}
+	if (scrollTop >= window.outerHeight * 0.6){
+		$(".cover-scroll-down").addClass("hidden");
+	}else{
+		$(".cover-scroll-down").removeClass("hidden");
+	}
+}
+export const bannerCoverInit = () => {
+	if ($("html").hasClass("banner-as-cover")){
+		classInit();
+		new MutationObserver(function(mutations, observer){
+			classInit();
+		}).observe(document.querySelector("#primary"), {
+			'childList': true
+		});
+		$(".cover-scroll-down").on("click" , function(){
+			gotoHash("#content", 600, 'easeOutCirc');
+			$("#content").focus();
+		});
+		
+		changeWidgetsDisplayStatus();
+	
+		$(window).scroll(function(){
+			changeWidgetsDisplayStatus();
+		});
+		$(window).resize(function(){
+			changeWidgetsDisplayStatus();
+		});
+	}
+};
+document.addEventListener("DOMContentLoaded", function(){
+	bannerCoverInit();
+});
