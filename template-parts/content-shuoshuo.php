889c1f6987f34eebf864fd24f98affa78ae22fbe
northword <northword@outlook.com>
2022-05-04 15:12:27 +0800

    fix passwd required not show in single shuoshuo

--- template-parts/content-shuoshuo.php
+++ template-parts/content-shuoshuo.php
@@ -5,21 +5,21 @@
 	<article class="card shuoshuo-main shuoshuo-foldable bg-white shadow-sm border-0" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<?php 
 			do_action( 'argon_entry_title' ); 
-			do_action( 'argon_entry_excerpt' );
+			if( is_singular( 'shuoshuo' ) ){
+				do_action( 'argon_entry_content' );
+			}else{
+				do_action( 'argon_entry_excerpt' );
+				global $withcomments;
+				$withcomments = true;
+				comments_template( '/comments-shuoshuo-preview.php' );
+	
+			}
 		?>
 
 		<!-- <div class="shuoshuo-content">
 			<?php the_content(); ?>
 		</div> -->
 		
-		<?php
-		// 若非说说详情页，则显示评论预览
-		if( !is_singular( 'shuoshuo' ) ):
-			global $withcomments;
-			$withcomments = true;
-			comments_template( '/comments-shuoshuo-preview.php' );
-			endif
-		?>
 
 		<?php
 			get_template_part( 'template-parts/shuoshuo', 'operations' );
